cadda10d0483ad11b8f09b09cb17861c174e516a
Jess <nomenjanaharimananajessica@gmail.com>
2024-03-22 00:08:44 +0300

    Config JSF et ajout CustemerList

---
+++ src/main/java/com/company/tpcustemersapp11/jsf/CustomerBean.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.company.tpcustemersapp11.jsf;
+
+import com.company.tpcustemersapp11.entity.Customer;
+import com.company.tpcustemersapp11.service.CustomerManager;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Backing bean de la page customerList.xhtml.
+ *
+ * @author USER
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    private List<Customer> customerList;
+
+    @Inject
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    public CustomerBean() {
+    }
+
+    /**
+     * Retourne la liste des clients pour affichage dans une DataTable.
+     * @return 
+     */
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomers();
+        }
+        return customerList;
+    }
+
+}
